7bc326732263cce33af273dfae376760e6f197c4
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-10-04 00:15:31 +0530

    Initial commit. pip installing opencv-python. importing cv2. imread ad imwrite(name, arrayobj)

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+
+# For opencv2 library the colors are Blue Green Red.
+# Ganesha = cv2.imread("Ganesha.png")
+# print(Ganesha)
+#
+#
+# data = cv2.imwrite("Ganesharocks.png", Ganesha)
+#
+# new=cv2.imread("Untitled.png")
+# print(new)
+
+new2=cv2.imread("new2.png")
+print(new2)
